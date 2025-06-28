992ead15fd7dbf8d7b0ee02bde28af0499dd2744
saivl2 <supriyavadali123@gmail.com>
2025-06-28 10:28:04 +0530

    added loops

---
+++ 02-Control Flow/range.py
@@ -0,0 +1,4 @@
+print(list(range(2, 45)))
+print()
+print(list(range(10, 0, -2)))
+
---
+++ 02-Control Flow/while-loops.py
@@ -0,0 +1,11 @@
+
+i = 0
+while i < 50:
+    print(i)
+    i +=1
+print()
+while True:
+    text = input()
+    if text == 'bye':
+        break
+    
